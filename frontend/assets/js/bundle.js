c07651f26d540122ee09ca345a1a6f7f11a5cfe4
Ivo Oliveira <ivo793@gmail.com>
2024-01-09 23:46:58 -0300

    Update bundle.js

--- frontend/assets/js/bundle.js
+++ frontend/assets/js/bundle.js
@@ -1,12 +1,11 @@
-/******/ (() => { // webpackBootstrap
-/******/ 	"use strict";
-var __webpack_exports__ = {};
-/*!******************************!*\
+/******/ (() => {
+  // webpackBootstrap
+  /******/ 'use strict';
+  var __webpack_exports__ = {};
+  /*!******************************!*\
   !*** ./src/webpack/index.ts ***!
   \******************************/
 
-console.log('Hello world!');
-
-/******/ })()
-;
+  /******/
+})();
 //# sourceMappingURL=bundle.js.map
